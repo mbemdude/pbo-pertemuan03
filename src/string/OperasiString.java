a4c6377fe83f0564511dee095a85e7d38210317c
mbemdude <ala52798@gmail.com>
2023-05-08 20:07:15 +0800

    Initial commit

---
+++ src/string/OperasiString.java
@@ -0,0 +1,12 @@
+package string;
+
+public class OperasiString {
+    public static void main(String[] args) {
+        String nama = "Ahmad";
+        int usia = 20;
+
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
+
+        System.out.println(nama);
+    }
+}
